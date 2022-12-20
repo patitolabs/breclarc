f240367978e7703521e053f7b54a47aafc1be3c0
Nícolas Castillo <72332975+ncr6@users.noreply.github.com>
2022-12-20 11:12:40 -0500

    feat: update git repo footer url

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,7 +7,7 @@
                     <div class="flex items-center">
                         <div class="ml-4">
                             <p class="mt-1 text-sm leading-5 text-neutral-500 dark:text-neutral-400">
-                                {{ config('app.name', 'Breclarc') }} {{ __('navigation.powered_by') }} <a href="https://git.ncastillo.xyz/infra/breclarc" class="text-neutral-700 hover:text-neutral-600 dark:text-neutral-300" target="_blank">Breclarc</a>
+                                {{ config('app.name', 'Breclarc') }} {{ __('navigation.powered_by') }} <a href="https://github.com/patitolabs/breclarc" class="text-neutral-700 hover:text-neutral-600 dark:text-neutral-300" target="_blank">Breclarc</a>
                             </p>
                         </div>
                     </div>
